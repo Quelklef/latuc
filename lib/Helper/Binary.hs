b708b0e4804397582702514955f484b6180777aa
Maynard <me@maynards.site>
2022-10-12 10:50:05 -0700

    factor out character mappings into module

--- lib/Helper/Binary.hs
+++ lib/Helper/Binary.hs
@@ -5,30 +5,12 @@ import qualified Helper.Unary    as Unary
 import           Data.Char       (GeneralCategory (ConnectorPunctuation, OtherNumber),
                                   generalCategory, isDigit, isLetter)
 import           Data.List.Extra (trim)
-import           Data.Map        (Map)
 import qualified Data.Map        as Map
 import           Data.Set        (Set)
 import qualified Data.Set        as Set
 
+import qualified Helper.Mappings as Mappings
 
-frac :: Map (String, String) String
-frac = Map.fromList
-  [ (("1", "2"), "½")
-  , (("1", "3"), "⅓")
-  , (("2", "3"), "⅔")
-  , (("1", "4"), "¼")
-  , (("3", "4"), "¾")
-  , (("1", "5"), "⅕")
-  , (("2", "5"), "⅖")
-  , (("3", "5"), "⅗")
-  , (("4", "5"), "⅘")
-  , (("1", "6"), "⅙")
-  , (("5", "6"), "⅚")
-  , (("1", "8"), "⅛")
-  , (("3", "8"), "⅜")
-  , (("5", "8"), "⅝")
-  , (("7", "8"), "⅞")
-  ]
 
 shouldParenthesizeStringWithChar :: Char -> Bool
 shouldParenthesizeStringWithChar c =
@@ -46,7 +28,7 @@ maybeParenthesize s =
 makeFraction :: String -> String -> String
 makeFraction (trim -> num) (trim -> den) =
   if (num, den) == ("", "") then ""
-  else case Map.lookup (num, den) frac of
+  else case Map.lookup (num, den) Mappings.frac of
           Just s -> s
           Nothing -> "(" <> maybeParenthesize num <> "/" <> maybeParenthesize den <> ")"
 
--- lib/Helper/Style.hs
+++ lib/Helper/Style.hs
@@ -1,26 +1,18 @@
 module Helper.Style where
 
-import           Data.Map     (Map)
-import qualified Data.Map     as Map
-import           Data.Set     (Set)
-import qualified Data.Set     as Set
+import qualified Data.Map        as Map
+import           Data.Set        (Set)
+import qualified Data.Set        as Set
 
-import qualified Helper.Unary as Unary
+import qualified Helper.Mappings as Mappings
+import qualified Helper.Unary    as Unary
 
-
-alias :: Map String String
-alias = Map.fromList
-  [ ("\\bf", "\\textbf")
-  , ("\\cal", "\\textcal")
-  , ("\\it", "\\textit")
-  , ("\\tt", "\\texttt")
-  ]
 
 names :: Set String
-names = Set.fromList (Map.keys alias)
+names = Set.fromList (Map.keys Mappings.alias)
 
 translate :: String -> String -> Either String String
 translate command text =
-  case Map.lookup command alias of
+  case Map.lookup command Mappings.alias of
     Nothing   -> Left $ "Unknown command: " <> command
     Just cmd' -> Unary.translate cmd' text
